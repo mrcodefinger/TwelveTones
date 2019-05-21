7a23722a99b3ff290203caf8426a646e65891b8b
Gabriel Serwas <post@gabrielserwas.com>
2019-05-21 21:56:25 +0200

    Add annotations.

--- index.php
+++ index.php
@@ -1,52 +1,83 @@
 <?php
 
+/**
+ * Class RandomValue
+ */
 class RandomValue {
+	/**
+	 * @var array
+	 */
+	private $values;
 
-  private $values;
+	/**
+	 * RandomValue constructor.
+	 *
+	 * @param array $values
+	 */
+	public function __construct( array $values ) {
+		$this->values = $values;
+	}
 
-  public function __construct(array $values) {
-    $this->values = $values;
-  }
-
-  public function __toString() {
-    return $this->values[rand(0, count($this->values) - 1)];
-  }
+	/**
+	 * @return string
+	 */
+	public function __toString() {
+		return $this->values[ rand( 0, count( $this->values ) - 1 ) ];
+	}
 }
 
+/**
+ * Class ShuffleArray
+ */
 class ShuffleArray {
 
-  private $values;
+	/**
+	 * @var array
+	 */
+	private $values;
 
-  public function __construct(array $values) {
-    $this->values = $values;
+	/**
+	 * ShuffleArray constructor.
+	 *
+	 * @param array $values
+	 */
+	public function __construct( array $values ) {
+		$this->values = $values;
 
-  }
+	}
 
-  public function __toString() {
-    return join(" ", $this->getValue());
-  }
+	/**
+	 * @return string
+	 */
+	public function __toString() {
+		return join( " ", $this->getValue() );
+	}
 
-  public function getValue() {
-    $values = $this->values;
-    shuffle($values);
-    return $values;
-  }
+	/**
+	 * @return array
+	 */
+	public function getValue() {
+		$values = $this->values;
+		shuffle( $values );
+
+		return $values;
+	}
 }
 
-$tones =  new ShuffleArray([
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  new RandomValue(["Ab", "G#"]),
-  new RandomValue(["Bb", "A#"]),
-  new RandomValue(["C#", "Db"]),
-  new RandomValue(["D#", "Eb"]),
-  new RandomValue(["F#", "Gb"]),
-]);
+$tones = new ShuffleArray( [
+	"A",
+	"B",
+	"C",
+	"D",
+	"E",
+	"F",
+	"G",
+	new RandomValue( [ "Ab", "G#" ] ),
+	new RandomValue( [ "Bb", "A#" ] ),
+	new RandomValue( [ "C#", "Db" ] ),
+	new RandomValue( [ "D#", "Eb" ] ),
+	new RandomValue( [ "F#", "Gb" ] ),
+] );
 
 //array_map("printf", $tones->getValue());
 
